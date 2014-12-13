2667be736a090666c025908df5de413730701dd4
Alexander <sharrikk@gmail.com>
2014-12-13 22:14:21 +0300

    init

---
+++ HW_NSURLSession/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HW_NSURLSession
+//
+//  Created by Alexander on 13.12.14.
+//  Copyright (c) 2014 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
